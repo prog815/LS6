a9966354339c9f0446baf7ef81eaf2bc4b3cfc24
prog <evprog@gmail.com>
2024-06-05 22:16:52 +0500

    обучает модель

--- web/train_model.py
+++ web/train_model.py
@@ -26,8 +26,28 @@ with open('queries.txt', 'w') as f:
         f.write(f"{hit['_source']['query']}\n")
 print("Выгрузка запросов завершена.")
 
+# Load the text file with queries
+queries = []
+with open('queries.txt', 'r') as f:
+    queries = [line.strip() for line in f.readlines()]
 
+# Check if the list of queries is not empty
+if not queries:
+    print("List of queries is empty. Skipping training.")
+    exit()
+
+# Предобработка данных: токенизация
+tokenized_data = [word_tokenize(phrase.lower()) for phrase in queries]
+                  
+# Подготовка данных в формате, подходящем для обучения Doc2Vec
+tagged_data = [TaggedDocument(words=words, tags=[str(i)]) for i, words in enumerate(tokenized_data)]
+                
 # Обучение модели
 print("Обучение модели...")
-# time.sleep(10)  # Симуляция обучения модели
+
+model = Doc2Vec(vector_size=100, window=5, min_count=1, workers=4, epochs=20)
+model.build_vocab(tagged_data)
+model.train(tagged_data, total_examples=model.corpus_count, epochs=model.epochs)
+model.save('model.bin')
+
 print("Обучение модели завершено.")
