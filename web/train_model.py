98790d007412ddf39481f0ac0a3f1efd41257a1b
prog <evprog@gmail.com>
2024-06-05 20:25:47 +0500

    выгрузка списка запросов и убрали подсказки в продакте

---
+++ web/train_model.py
@@ -0,0 +1,29 @@
+import os
+import time
+from elasticsearch import Elasticsearch
+from datetime import datetime
+from flask import jsonify
+
+
+el_cnn = 'elasticsearch:9200'
+el_name = 'elastic'
+el_pass = 'abc123456'
+
+# Выгрузка запросов в текстовый файл
+print("Выгрузка запросов...")
+with open('queries.txt', 'w') as f:
+    es = Elasticsearch([el_cnn], http_auth=(el_name, el_pass))
+    query_body = {
+        "query": {
+            "match_all": {}
+        }
+    }
+    results = es.search(index='suggests', body=query_body)
+    for hit in results['hits']['hits']:
+        f.write(f"{hit['_source']['query']}\n")
+print("Выгрузка запросов завершена.")
+
+# Обучение модели
+print("Обучение модели...")
+# time.sleep(10)  # Симуляция обучения модели
+print("Обучение модели завершено.")
